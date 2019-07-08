061f2183cecf62fea6ea1ba4cf552e8947a592d7
Esteban Victorio <estebanvictorio92@gmail.com>
2019-07-07 22:03:53 -0500

    [fix] fix conflict

--- packages/ftn-react/next.config.js
+++ packages/ftn-react/next.config.js
@@ -1,8 +1,4 @@
 const prod = process.env.NODE_ENV === 'production'
 module.exports = {
-<<<<<<< HEAD
-  assetPrefix: '',
-=======
   assetPrefix: prod ? 'http://evictorio.com/ftn-react' : ''
->>>>>>> 85e61db2288224620415e9e4ae76a28156e02192
 }
