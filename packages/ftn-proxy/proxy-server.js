f9beb8ab71436eb236f5bb3044e34ee9a5a18834
Esteban Victorio <estebanvictorio92@gmail.com>
2019-07-08 12:49:00 -0500

    [update] add routes on proxy server middleware

--- packages/ftn-proxy/proxy-server.js
+++ packages/ftn-proxy/proxy-server.js
@@ -1,16 +1,27 @@
-let express = require('express')
-let proxy= require('http-proxy-middleware')
-const PORT = 3000
-let httpProxy = proxy({ 
+const express = require('express')
+const proxy = require('http-proxy-middleware')
+const PORT = process.env.PORT || 3000
+
+const REACT_BLOG = '/ftn-react'
+const JS_BLOG = '/ftn-javascript'
+const NEXT_JS_BLOG = '/ftn-next'
+
+
+
+let httpProxy = proxy({
   target: 'http://localhost:4000',
   changeOrigin: true,
-  pathRewrite: {
-    '/app2': '/'
+  router: {
+    '/ftn-javascript':'http://localhost:4001',
+    '/ftn-react': 'http://localhost:4002',
+    '/ftn-next': 'http://localhost:4003'
   }
 })
 
 
 let app = express()
-console.log(httpProxy)
-app.use('/app2', httpProxy)
+app.use(NEXT_JS_BLOG, httpProxy)
+app.use(REACT_BLOG, httpProxy)
+app.use(JS_BLOG, httpProxy)
+
 app.listen(PORT)
